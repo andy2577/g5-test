06866fcfcc91f20939d73249ac0133722462d9e9
Andriy <andy25771@gmail.com>
2019-07-18 08:55:31 +0300

    project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { PageBlocksComponent } from './components/page-blocks/page-blocks.component';
+import { PageTableComponent } from './components/page-table/page-table.component';
+import { PageDetailComponent } from './components/page-detail/page-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    NavigationBarComponent,
+    PageBlocksComponent,
+    PageTableComponent,
+    PageDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/page-blocks/page-blocks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-blocks',
+  templateUrl: './page-blocks.component.html',
+  styleUrls: ['./page-blocks.component.scss']
+})
+export class PageBlocksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
